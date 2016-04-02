da407226dfe6278463465661d3ac44c0b180b627
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-04-02 10:47:23 -0700

    In-place progress updates
    Counter & current app being scanned.

--- sparkler.rb
+++ sparkler.rb
@@ -1,13 +1,20 @@
-puts "Scanning..."
-#apps = `find /Applications -name "*.app"`
+require 'pty'
+puts "Beginning scan."
+
 res = []
-IO.popen('find /Applications -name "*.app"').each do |line|
-  p line.chomp
-  res << line.chomp
-  puts res.count
+PTY.spawn 'find /Applications -name "*.app"' do |r,w,p|
+  while r.gets.is_a?String do
+    res << r.gets
+
+     puts "\r#{res.count} apps found..."
+     puts "#{r.gets.rstrip.ljust(80)[0...80]}"
+     print "\033[2A"
+    
+  end
 end
+print 'done.'
 
-
+puts "\nDeduping apps..."
 apps = []
 res.each do |ln|
   item = Hash[:path, ln.strip, :app, "#{ln.split('.app')[0]}.app"]
@@ -16,4 +23,4 @@ end
 
 apps = apps.uniq {|item| item[:app]}
 #apps.each {|app| puts app[:app]}
-puts "Found #{apps.count} apps in /Applications."
+puts "Done.  Found #{apps.count} apps in /Applications."
