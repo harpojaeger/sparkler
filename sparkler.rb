776d899029965c61dbca28a4f28c22d954a5ba24
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-04-02 11:09:31 -0700

    Live-updater works, doesn't skip results

--- sparkler.rb
+++ sparkler.rb
@@ -1,26 +1,31 @@
 require 'pty'
-puts "Beginning scan."
+puts 'Starting scan.'
 
 res = []
-PTY.spawn 'find /Applications -name "*.app"' do |r,w,p|
-  while r.gets.is_a?String do
-    res << r.gets
 
-     puts "\r#{res.count} apps found..."
-     puts "#{r.gets.rstrip.ljust(80)[0...80]}"
-     print "\033[2A"
-    
-  end
+PTY.spawn 'find /Applications -name "*.app"' do |r, _w, _p|
+  puts ''
+  puts ''
+
+  while (ln = r.gets).is_a?String
+
+    res << ln
+    print "\033[2A"
+    puts "\r#{res.count} apps found..."
+    puts (ln.strip.ljust(80)[0...80])
+
+    end
 end
-print 'done.'
 
-puts "\nDeduping apps..."
+print "\033[1A"
+puts 'Done.'.ljust(80)
+puts 'Deduping apps...'
 apps = []
 res.each do |ln|
   item = Hash[:path, ln.strip, :app, "#{ln.split('.app')[0]}.app"]
   apps.push(item)
 end
 
-apps = apps.uniq {|item| item[:app]}
-#apps.each {|app| puts app[:app]}
+apps = apps.uniq { |item| item[:app] }
+# apps.each {|app| puts app[:app]}
 puts "Done.  Found #{apps.count} apps in /Applications."
