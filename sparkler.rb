5dc0ba516e221e59fca74f77e6c171d0773bfa13
Harpo Jaeger <harpo.jaeger@gmail.com>
2016-04-02 18:46:50 -0700

    Much DRYer
    Make `find` do the heavy lifting on looking for Sparkle.framework (was
    worried about apps w/ other .app folders in them, but they probably
    won't have their own Sparkle instances), and combine hash maker into
    the loop that reads stdout from `find`.

--- sparkler.rb
+++ sparkler.rb
@@ -1,42 +1,30 @@
 require 'pty'
 termwidth = 80
-scanpath = '/Applications'
-sparklepath = 'Contents/Frameworks/Sparkle.framework'
+scanpath = '/Applications/'
 
 puts 'Scanning...'
-res = []
-2.times { puts }
-PTY.spawn "find #{scanpath} -name '*.app'" do |r, _w, _p|
-  while (ln = r.gets).is_a?String
+apps = []
 
-    res << ln
+PTY.spawn "find #{scanpath} -name 'Sparkle.framework'" do |r, _w, _p|
+  2.times { puts }
+  while (ln = r.gets).is_a?String
+    basepath = ln.split('.app')[0]
+    basename = basepath.split(scanpath)[1]
+
+    item = Hash[
+    :sparkle, ln.strip,
+    :path, "#{basepath}.app",
+    :name, "#{basename}.app"
+    ]
+    apps << item
     print "\033[2A"
-    puts "\r#{res.count} apps found..."
-    puts ln.strip.ljust(termwidth)[0...termwidth]
+    puts "\r#{apps.count} sparkly apps found..."
+    puts item[:name].ljust(termwidth)[0...termwidth]
 
   end
 end
 
 print "\033[3A"
 puts 'Scanning...done.'.ljust(termwidth)
-dedupe_msg = 'Deduping apps...'
-print dedupe_msg
-apps = []
-res.each do |ln|
-  item = Hash[
-  :path, ln.strip,
-  :app, "#{ln.split('.app')[0]}.app",
-  :sparkly, false
-  ]
-  apps << item
-end
-
-apps = apps.uniq { |item| item[:app] }
-# apps.each {|app| puts app[:app]}
-puts 'done.'.ljust(termwidth - dedupe_msg.length)
-puts "Found #{apps.count} apps in /Applications.".ljust(termwidth)
-print 'Checking for Sparkle.framework...'
-apps.each { |app| app[:sparkly] = File.exist?("#{app[:path]}/#{sparklepath}") }
-sparkly_apps = apps.select { |app| app[:sparkly] }
-puts 'done.'
-puts "#{sparkly_apps.count} apps with Sparkle."
+puts "Found #{apps.count} sparkly apps in #{scanpath}.".ljust(termwidth)
+puts ''
